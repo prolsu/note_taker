77e86555d879e0e7657deeb765b490421cb0caf6
Pedro Alburqueque <prolsu88@gmail.com>
2020-12-17 23:03:33 -0500

    commit to heroku

--- Develop/server.js
+++ Develop/server.js
@@ -14,6 +14,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // HTML Routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
